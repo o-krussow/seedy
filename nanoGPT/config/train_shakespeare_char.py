f095512a4b9e2a66e98de47acd8606a587086123
o936k099 <o936k099@hermes.ittc.ku.edu>
2024-04-13 00:43:32 -0500

    test spot

--- nanoGPT/config/train_shakespeare_char.py
+++ nanoGPT/config/train_shakespeare_char.py
@@ -14,7 +14,7 @@ wandb_project = 'shakespeare-char'
 wandb_run_name = 'mini-gpt'
 
 dataset = 'shakespeare_char'
-gradient_accumulation_steps = 1
+gradient_accumulation_steps = 2
 batch_size = 64
 block_size = 256 # context of up to 256 previous characters
 
@@ -25,8 +25,8 @@ n_embd = 384
 dropout = 0.2
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 15000
-lr_decay_iters = 15000 # make equal to max_iters usually
+max_iters = 50000
+lr_decay_iters = 50000 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
--- nanoGPT/data/shakespeare/prepare.py
+++ nanoGPT/data/shakespeare/prepare.py
@@ -6,7 +6,7 @@ import numpy as np
 # download the tiny shakespeare dataset
 input_file_path = os.path.join(os.path.dirname(__file__), 'input.txt')
 if not os.path.exists(input_file_path):
-    data_url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt'
+    data_url = 'https://raw.githubusercontent.com/o-krussow/seedy/main/articles_not.txt'
     with open(input_file_path, 'w', encoding='utf-8') as f:
         f.write(requests.get(data_url).text)
 
