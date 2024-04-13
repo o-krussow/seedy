a4caeced91fdbe8a1b3a2977a78e2b905cfe34dc
c710h797 <c710h797@hermes.ittc.ku.edu>
2024-04-13 01:42:59 -0500

    orginization

--- nanoGPT/data/shakespeare_char/prepare.py
+++ nanoGPT/data/shakespeare_char/prepare.py
@@ -12,7 +12,7 @@ import numpy as np
 # download the tiny shakespeare dataset
 input_file_path = os.path.join(os.path.dirname(__file__), 'input.txt')
 if not os.path.exists(input_file_path):
-    data_url = 'https://raw.githubusercontent.com/o-krussow/seedy/main/all_articles.txt'
+    data_url = 'https://raw.githubusercontent.com/o-krussow/seedy/main/articles/test_clean.txt'
     with open(input_file_path, 'w') as f:
         f.write(requests.get(data_url).text)
 
