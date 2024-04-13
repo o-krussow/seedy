59a812c2a4240bcba1861a4d9c59591369456973
o936k099 <o936k099@hermes.ittc.ku.edu>
2024-04-13 11:46:27 -0500

    api server basic functionality is there

---
+++ seedy/apiserver.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify
+from main import headline_generator
+from main import paragraph_generator
+from main import image_generator
+
+
+app = Flask(__name__)
+
+def get_article(prompt=""):
+    tasks = [
+        {
+            'headline': headline_generator(prompt),
+            'paragraph': paragraph_generator(),
+            'image': image_generator(),
+        },
+    ]
+    return tasks
+
+@app.route('/api', methods=['GET'])
+def get_article_without_input():
+    return jsonify({'article': get_article()})
+
+@app.route('/api', methods=['POST'])
+def get_article_with_input():
+    if request.is_json:
+        prompt_json = request.get_json()
+        prompt = prompt_json["prompt"]
+
+        print(prompt)
+        
+        return jsonify({'article': get_article(prompt)}), 201
+    return {"error": "Request must be JSON"}, 415
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
