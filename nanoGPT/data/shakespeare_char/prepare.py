a30fd41590ade2dd22d8df174350f09cdedc202f
o936k099 <o936k099@hermes.ittc.ku.edu>
2024-04-12 20:49:47 -0500

    Some changes

--- nanoGPT/data/shakespeare_char/prepare.py
+++ nanoGPT/data/shakespeare_char/prepare.py
@@ -11,10 +11,10 @@ import numpy as np
 
 # download the tiny shakespeare dataset
 input_file_path = os.path.join(os.path.dirname(__file__), 'input.txt')
-if not os.path.exists(input_file_path):
-    data_url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt'
-    with open(input_file_path, 'w') as f:
-        f.write(requests.get(data_url).text)
+#if not os.path.exists(input_file_path):
+#    data_url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt'
+#    with open(input_file_path, 'w') as f:
+#        f.write(requests.get(data_url).text)
 
 with open(input_file_path, 'r') as f:
     data = f.read()
