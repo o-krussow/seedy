892e20826a4e4559289063cac9b7c9cef95c8d93
o936k099 <o936k099@hermes.ittc.ku.edu>
2024-04-13 11:51:01 -0500

    added comment and removed print

--- seedy/apiserver.py
+++ seedy/apiserver.py
@@ -24,10 +24,11 @@ def get_article_without_input():
 def get_article_with_input():
     if request.is_json:
         prompt_json = request.get_json()
-        prompt = prompt_json["prompt"]
 
-        print(prompt)
+        #Post should come in format {prompt: <the prompt>}
+        prompt = prompt_json["prompt"]
         
+        #Pass prompt to get_article, which will pass prompt to headline generator
         return jsonify({'article': get_article(prompt)}), 201
     return {"error": "Request must be JSON"}, 415
 
