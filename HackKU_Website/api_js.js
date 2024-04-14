861ea159fe80d041aab67e66fa673e6e29a14a73
= <=>
2024-04-14 04:32:13 -0500

    added correct image

--- HackKU_Website/api_js.js
+++ HackKU_Website/api_js.js
@@ -1,15 +1,49 @@
-console.log("running")
+//document.addEventListener('load', makeObjs, false);
+document.getElementById("getdata").addEventListener('click', getData, false);
 
-let button = document.getElementById("mybutton")
+var myTeam = [];
 
-button.addEventListener("click", do_operation, false)
+function assembleTeam() {
 
-function do_operation() {
-    let a = "hello world"
-    console.log(a)
+    document.getElementById("teamTXT").innerHTML = "Your team:";
+    document.getElementById("getdata").value = "Re-roll your team!";
 
-    let obj = document.getElementById("test")
-    obj.innerHTML = a;
-    // console.log(a)
+    for (var i = 0; i < 6; i++) {
+
+        var poke = document.createElement("Object");
+
+        poke.name = myTeam[i].name;
+        poke.src = myTeam[i].sprites.front_default;
+
+        document.getElementById("teamn" + i).innerHTML = poke.name;
+        document.getElementById("teami" + i).src = poke.src;
+    }
+
+    //poke1.style.boarder = "5px solid red";
+    //poke1.style.width = "900px";
+
 }
 
+
+//Application Programming Interface
+function getData() {
+    myTeam = [];
+    var randomNum;
+
+    var requestObj = new XMLHttpRequest();
+    randomNum = Math.round(Math.floor(Math.random() * 891 + 1));
+    requestObj.addEventListener('load', stickInArray, false);
+    requestObj.open("GET", "https://localhost:5000/api/");
+    console.log(requestObj);
+    requestObj.send();
+
+
+    function stickInArray(e) {
+        console.log(myTeam);
+        myTeam.push(JSON.parse(e.currentTarget.response));
+        if (myTeam.length == 6) { assembleTeam(); }
+    }
+}
+
+
+// curl http://localhost:5000/api
